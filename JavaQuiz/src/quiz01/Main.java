aed52855bebad71c924b7a0d2ee0fb421918ae8d
user <user@BOOK-LCCUUB9T5H>
2024-05-22 17:52:51 +0900

    .

--- JavaQuiz/src/quiz01/Main.java
+++ JavaQuiz/src/quiz01/Main.java
@@ -15,25 +15,26 @@ public class Main { // 클래스명 반드시 Main
 
 		Scanner scan = new Scanner(System.in);
 
-		int num1 = 472;
-		int num2 = 385;
+		int num1 = scan.nextInt();
+		int num2 = scan.nextInt();
 
-		int a1 = num1 % 10;
-		int a2 = num1 % 100 - a1;
-		int a3 = num1 % 1000 - a1 - a2;
+		int a1 = num1 % 10; // 2
+		int a2 = num1 % 100 - a1; // 70
+		int a3 = num1 % 1000 - a1 - a2; // 400
 
-		int b1 = num2 % 10;
-		int b2 = num2 % 100 - b1;
-		int b3 = num2 % 1000 - b1 - b2;
+		int b1 = num2 % 10; // 5
+		int b2 = num2 % 100 - b1; // 80
+		int b3 = num2 % 1000 - b1 - b2; // 300
 
-		int c1 = a1 * b1;
-		int c2 = a2 * b2;
-		int c3 = a3 * b3;
+		int c1 = (a1 * b1) + (a2 * b1) + (a3 * b1);
+		int c2 = (a1 * b2) + (a2 * b2) + (a3 * b2);
+		int c3 = (a1 * b3) + (a2 * b3) + (a3 * b3);
 
-		System.out.println(a1 * b1);
-		System.out.println(a2 * b2);
-		System.out.println(a3 * b3);
+		System.out.println(c1);
+		System.out.println(c2 /10);
+		System.out.println(c3 / 100);
 		
+		System.out.println(c1+ c2+ c3);
 
 	}
 }
