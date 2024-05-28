22041cb1cd320538871320c4babc116fa9a56bce
user <user@BOOK-LCCUUB9T5H>
2024-05-28 17:12:59 +0900

    interface quiz

---
+++ JavaQuiz/src/quiz10/Interface/MelonMusic.java
@@ -0,0 +1,16 @@
+package ??;
+
+public class MelonMusic {
+
+	private String[] list = new String[100];
+	private int count = 0;
+	
+	/*
+	 * SongList인터페이스를 상속받아서 기능을 구현합니다.
+	 * insertList() 는 list배열에 순서대로 저장
+	 * playList() list의 음악을 랜덤하게 출력
+	 * playLength() 는 저장된 음악의 개수를 반환
+	 */
+	
+	
+}
---
+++ JavaQuiz/src/quiz10/Interface/SongList.java
@@ -0,0 +1,8 @@
+package ??;
+
+public interface SongList {
+
+	public void insertList(String song);
+	public void playList();
+	public int playLength();
+}
