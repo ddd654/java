f4737d8a17c63475facdac9de06c056e1c23d04b
user <user@BOOK-LCCUUB9T5H>
2024-05-30 17:51:43 +0900

    .

--- JavaQuiz/src/quiz01/Main.java
+++ JavaQuiz/src/quiz01/Main.java
@@ -22,7 +22,7 @@ import java.util.StringTokenizer;
 // car는 Car의 객체이다.
 
 //
-//public static void main(String[] args) throws IOException { //메인함수 예외발생 처리? 이게 뭐지
+// public static void main(String[] args) throws IOException { //메인함수 예외발생 처리? 이게 뭐지
 //	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //	BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 //	
@@ -41,29 +41,22 @@ public class Main {
 	// main 메서드
 	public static void main(String[] args) throws IOException { // 메인함수 예외발생 처리? 이게
 
-		// 괄호안에다가 System.in으로 콘솔창에서 읽을건지, 파일로 읽는지 등 넣기
-		// 스캐너처럼 틀만들기
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-		// String으로 read하고 int로 변환한다
-
 		int t = Integer.parseInt(br.readLine());
-
-		StringTokenizer st;
+		
+		
 
 		for (int i = 0; i < t; i++) {
-			//공백을 구분
-			st = new StringTokenizer(br.readLine()," ");
+			StringTokenizer token = new StringTokenizer(br.readLine(), " ");
 
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
+			int a = Integer.parseInt(token.nextToken(br.readLine(), " "));
+			int b = Integer.parseInt(token.nextToken(br.readLine(), " "));
 
-			bw.write(a + b + "\n"); // 입력 하기, \n 줄바꿈이다
-
+			System.out.println("Case #"+"x: " + a + b);
 		}
-		bw.flush(); // 콘솔창에 띄우기, 위에랑 같이
-		br.close(); // 닫기
+
 	}
 
 }
