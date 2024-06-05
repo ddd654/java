722fca34a32832efae19332bdf73fc02bafa1ca2
user <user@BOOK-LCCUUB9T5H>
2024-06-05 17:19:56 +0900

    Regex_Quiz

---
+++ JavaQuiz/src/quiz19/Quiz_Regex03.java
@@ -0,0 +1,13 @@
+package quiz19;
+
+public class Quiz_Regex03 {
+	public static void main(String[] args) {
+		
+		// buffered 사용해서 읽는다
+		// 정규표현식 사용해서 일련번호, 지점(건담베이스 강남),
+		// 등급[등급], 상세내용, 가격 으로 5개 출력
+		
+		// 상세내용 정규표현식으로 찾기 불가능해서 시작, 끝지점으로 문자열 자르기
+		
+	}
+}
