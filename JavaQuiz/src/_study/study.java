e49dc2fb39e49079c4cb32e32b433fb5002d8607
user <user@BOOK-LCCUUB9T5H>
2024-06-05 18:09:40 +0900

    myquiz

--- JavaQuiz/src/_study/study.java
+++ JavaQuiz/src/_study/study.java
@@ -3,21 +3,30 @@ package _study;
 public class study {
 	public static void main(String[] args) {
 
-
 		System.out.println(solution(1, 2, 3, 4));
-		
+
 	}
-	
+
 	public static int[] solution(int numer1, int denom1, int numer2, int denom2) {
         int[] answer = {}; 
         
+        1   3   4  6  10
+        2   4   8  8   8s
+        
             if(denom1 < denom2){
-                answer[1] = 1/ 2;
+                int numer = (numer1 * denom2) + (numer2 * denom1); //분자 10
+                int denom = (denom1 * denom2); //분모                     8
                 
-            } else {
+            } else if (denom1 > denom2){
                 answer[0] = denom1;
-                
+                answer[1] = ;
+            } else { //같은경우
+            	answer[0] = numer1 +numer2;
+            	answer[1] = denom1;
             }
+        
+        
+        
             
         return answer;
     }
