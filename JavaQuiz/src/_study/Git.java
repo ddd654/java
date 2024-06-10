9bcafd92983a91d0bd66a3f02f41179ccf5e142a
chris <bound654@naver.com>
2024-06-10 17:12:46 +0900

    깃

--- JavaQuiz/src/_study/Git.java
+++ JavaQuiz/src/_study/Git.java
@@ -14,12 +14,28 @@ public class Git {
 	 * -첫 초기화, 한번만
 	 * git init
 	 *
+	 * -자격증명 확인하기
 	 * --------------------------------------
 	 * 
+	 * 쉬프트 + 인설트 하면 분여넣기
 	 * cd + 폴더 주소 = 적으면 그폴더로 인식
 	 * cd .. = 이전 경로 이동
 	 * clear 적으면 창 글 제거 git --version
-
+	   git status
+	   git rm -- cached 파일명 > 스테이지에서 지우기
+	 * git commit -m 메시지 > 깃을 레포에 저장
+	 * 
+	 * git reset --hard HEAD^  < 모두 초기화
+	 * git reset --mixed HEAD^  < 수정한, 작성한 코드는 남는다
+	 * git reset --soft HEAD^  < 커밋만 리셋
+	 * ^ 한단계만 초기화한다는 표시
+	 *
+	 *  
+	 * -----------------------------------
+	 * 
+	 * .gitignore 필요없는 파일 필터링하기
+	 * 
+	 * 
 	 * 
 	 */
 }
--- JavaQuiz/src/_study/study.java
+++ JavaQuiz/src/_study/study.java
@@ -1,10 +1,27 @@
 package _study;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class study {
 	public static void main(String[] args) {
+
+		Scanner scan1 = new Scanner(System.in);
+		int a = scan1.nextInt();
+
+		// 배열 int[] arr 만들기
+		int[] arr = new int[a];
+
+		// 배열에 길이만큼, 1씩 넣기
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = i + 1;
+		}
+
+		System.out.println(arr);
+		System.out.println(Arrays.toString(arr));
 		
-		solution();
-		
+		System.out.println(solution(arr));
+
 	}
 
 	public static double solution(int[] numbers) {
