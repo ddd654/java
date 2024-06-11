0fb5eebdcfb3aa83de1e4141682ed4b7433bc8fc
chris <bound654@naver.com>
2024-06-11 09:20:52 +0900

    .

--- JavaQuiz/src/_study/study.java
+++ JavaQuiz/src/_study/study.java
@@ -1,38 +1,20 @@
 package _study;
 
-import java.util.Arrays;
-import java.util.Scanner;
-
 public class study {
 	public static void main(String[] args) {
 
-		Scanner scan1 = new Scanner(System.in);
-		int a = scan1.nextInt();
-
-		// 배열 int[] arr 만들기
-		int[] arr = new int[a];
-
-		// 배열에 길이만큼, 1씩 넣기
-		for (int i = 0; i < arr.length; i++) {
-			arr[i] = i + 1;
-		}
-
-		System.out.println(arr);
-		System.out.println(Arrays.toString(arr));
-		
-		System.out.println(solution(arr));
+		System.out.println(solution(5));
 
 	}
 
-	public static double solution(int[] numbers) {
-		double answer = 0;
+	public static int solution(int n) {
+		int answer = 0;
 
-		for (int i = 0; i <= numbers.length; i++) {
-			answer += numbers[i];
+		for (int i = 2; i <= n; i += 2) {
+			answer += i;
 		}
 
 		return answer;
-
 	}
 
 }
