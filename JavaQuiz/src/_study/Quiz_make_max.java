6c3b2c3f204a68c53403e9de829ff693b551a57a
chris <bound654@naver.com>
2024-06-12 17:50:54 +0900

    .

---
+++ JavaQuiz/src/_study/Quiz_make_max.java
@@ -0,0 +1,14 @@
+package _study;
+
+public class Quiz_make_max {
+	public static void main(String[] args) {
+		
+	}
+	
+	
+	public int solution(int[] numbers) {
+        int answer = 0;
+        return answer;
+    }
+	
+}
