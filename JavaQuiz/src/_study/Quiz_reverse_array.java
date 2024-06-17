4b256f701057fbbfd7d9fc6b5a1e643220be2a32
chris <bound654@naver.com>
2024-06-17 19:10:03 +0900

    .

---
+++ JavaQuiz/src/_study/Quiz_reverse_array.java
@@ -0,0 +1,21 @@
+package _study;
+
+import java.util.Arrays;
+
+public class Quiz_reverse_array {
+	public static void main(String[] args) {
+
+		int[] num_list = {};
+		int[] answer = solution(num_list);
+	    System.out.println(Arrays.toString(answer)); 
+	}
+
+	public static int[] solution(int[] num_list) {
+		int[] answer = {};
+		
+		
+		
+		return answer;
+	}
+
+}
