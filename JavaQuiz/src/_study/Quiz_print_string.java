9b8b883532056756c170544651ea5381d9b1ae7f
ddd654 <bound654@naver.com>
2024-07-11 08:40:06 +0900

    문자 반복 출력하기

--- JavaQuiz/src/_study/Quiz_print_string.java
+++ JavaQuiz/src/_study/Quiz_print_string.java
@@ -8,19 +8,20 @@ public class Quiz_print_string {
 	}
 
 	//my_string이 문자열, n이 횟수
-	public static char solution(String my_string, int n) {
-		char answer = 0;
+	public static String solution(String my_string, int n) {
+		String answer = "";
 		
 		//문자열을 한칸씩 나눠서 문자수만큼 반복하기
 		for(int i = 0; i < my_string.length(); i++) {
-//			answer(i) = my_string.charAt(i);
 
 			//그중에서 1글자씩 n번 반복하기
 			for(int j = 0; j < n ; j++) {
+				answer += my_string.charAt(i);
+
 			}
 			
 		}
 		
 		return answer;
-	}
+	} 
 }
