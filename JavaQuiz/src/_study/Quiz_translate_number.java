5ae3db2f7f04a92bd81b9312e33d6780faaf3c8a
ddd654 <bound654@naver.com>
2024-07-16 18:51:52 +0900

    .

--- JavaQuiz/src/_study/Quiz_translate_number.java
+++ JavaQuiz/src/_study/Quiz_translate_number.java
@@ -8,10 +8,33 @@ public class Quiz_translate_number {
 		// 들어 23살은 cd, 51살은 fb로 표현합니다. 나이 age가 매개변수로 주어질 때 PROGRAMMER-962식 나이를 return하도록
 		// solution 함수를 완성해주세요.
 
+		System.out.println(solution(123));
 	}
 
 	public static String solution(int age) {
 		String answer = "";
+		
+		//1이 b이 된다
+		// 숫자를 입력하면 알파벳문자가 나오게 하기		
+		String age2 = Integer.toString(age);
+		
+		
+		
+		//비교할 알파벳
+		String alpa = "a";
+		
+		//문자열 길이만큼 반복? 알파벳 개수만큼 반복?
+		for(int i = 0; i <= 26; i++) {
+			
+			//age2의 문자열 i번째를 i값 자체와 비교하기
+			if(age2.charAt(i) == i) {
+				answer += "a";
+				
+				
+			}
+
+		}
+
 		
 		
 		return answer;
